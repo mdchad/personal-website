0a8d3819c3d34bb6b96af13bcb4d6ab937440da3
mdchad <irsyad.muhd@gmail.com>
2022-10-24 13:37:15 +0800

    change name

--- src/lib/generateRssFeed.js
+++ src/lib/generateRssFeed.js
@@ -8,8 +8,8 @@ export async function generateRssFeed() {
   let articles = await getAllArticles()
   let siteUrl = process.env.NEXT_PUBLIC_SITE_URL
   let author = {
-    name: 'Spencer Sharp',
-    email: 'spencer@planetaria.tech',
+    name: 'Irsyad',
+    email: 'irsyad.muhd@gmail.com',
   }
 
   let feed = new Feed({
