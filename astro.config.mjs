68dee88c1e353b5cc7696a0962de55239f08e2ed
mdchad <irsyad.muhd@gmail.com>
2025-09-08 13:31:25 +0800

    add sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,17 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import tailwindcss from '@tailwindcss/vite';
+
+
+import sitemap from '@astrojs/sitemap';
+
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
+  integrations: [react(), sitemap()],
   output: 'static',
-
+  site: "https://irsyad.dev",
   build: {
     assets: '_assets'
   },
